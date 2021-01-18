259054e4e907fd81ac8ec1a2e59033c58e8ddb6b
phxtho <phathopukwana@gmail.com>
2021-01-19 00:40:15 +0200

    save output to file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::path::Path;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader,Write};
 
-fn parse_markdown_file(filename: &str) {
+fn parse_markdown_file(filename: &str) -> Vec<String> {
     println!("[INFO] Trying to parse {}...", filename);
 
     let input_filename = Path::new(filename);
@@ -58,19 +58,34 @@ fn parse_markdown_file(filename: &str) {
             _htag = false;
             output_line.push_str("</h1>\n");      
           } 
-          
+
           if output_line != "<p></p>\n" {
             tokens.push(output_line);
           }
 
      }
+
+     tokens
+}
+
+fn save_html_file(tokens: &Vec<String>, filename: &str) {
+    let mut output_filename = String::from(&filename[..filename.len()-3]); // remove ".md" from filename
+    output_filename.push_str(".html");
+    let mut outfile = File::create(output_filename).expect("[ERROR] Could not create ouput file");
+    for line in tokens {
+        outfile.write_all(line.as_bytes()).expect("[ERROR] Could not write output to file");
+    }
 }
 
 fn main() {
     let args:Vec<String> = std::env::args().collect();
     match args.len() {
         1 => (), 
-        2 => parse_markdown_file(&args[1]),
+        2 => {
+            let filename = &args[1];
+            let tokens = parse_markdown_file(filename);
+            save_html_file(&tokens, filename);
+        },
         _ => ()
     };
 }
